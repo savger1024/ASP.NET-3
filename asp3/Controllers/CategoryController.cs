eb582b3872ee5fffa5bd1dc97a7c1332d885bb95
savger1024 <savger1024@gmail.com>
2023-12-02 16:23:49 +0100

    Add project files.

---
+++ asp3/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using asp3.Data;
+using asp3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace asp3.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categories;
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ asp3/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using asp3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace asp3.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+
+}
